058257cddeeaa9a72665be11201ed4e9415f1155
Vuyolwethu <vngqaqhu@gmail.com>
2015-03-21 00:36:49 +0200

    function push github

---
+++ Tut3_TicTacToe/TestFunction.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include "TicTacToe.h"
+using namespace std;
+
+int main()
+{
+	TicTacToe tictactoe;
+	int n = 0;
+	n++;
+	tictactoe.printBoard();
+	while (1)
+	{
+		tictactoe.Input();
+		tictactoe.printBoard();
+		if (tictactoe.checkWinner() == 'X')
+		{
+			cout << "X Wins!" << endl;
+			break;
+		}
+		else if (tictactoe.checkWinner() == 'O')
+		{
+			cout << "O Wins!" << endl;
+			break;
+		}
+		else if (tictactoe.checkWinner() == '/' && n == 9)
+		{
+			cout << "The game was a draw." << endl;
+			break;
+		}
+		tictactoe.changePlayer();
+	}
+
+	system("pause");
+	return 0;
+
+}
